29ba7e72fa680146554c378dd5385352f4e41538
erikschlegel <erik.schlegel@gmail.com>
2015-06-06 11:55:43 -0400

    Initial commit

--- example.js
+++ example.js
@@ -3,6 +3,7 @@
 var React = require('react');
 var ReactTypeahead = require('./lib/js/react-typeahead');
 var demoDataCallRoot = 'http://demos.telerik.com/kendo-ui/';
+var Handlebars = require('handlebars');
 
 var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
@@ -15,23 +16,35 @@ var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
 ];
 
+var handlerbarTemplate = '<div class="tt-custom-row"> ' + 
+                                   '     <span class="tt-custom-cell tt-custom-thumbnail">' + 
+                                   '         <img src="{{thumbnail}}" />' + 
+                                   '     </span>' + 
+                                   '     <span class="tt-custom-cell">' +
+                                   '           <h3>{{value}}</h3>' + 
+                                   '           <p>{{description}}</p>' + 
+                                   '     </span>' +
+                                   ' </div>';
+
 var bloodhoundConfig = {
 	local: states
 };
 
 React.render(
     <ReactTypeahead bloodhound={bloodhoundConfig} 
-                    placeHolder="States in the U.S"/>,
+                    placeHolder="States - A basic example"/>,
     document.getElementById('#typeaheadDiv')
 );
 
 var responseTransformation = function(rsp){
-      var initRsp = rsp.items, maxCharacterLgth = 25;
+      var initRsp = rsp.items, maxCharacterTitleLgth = 29, maxDescLength = 80;
       var finalResult = [];
       
       initRsp.map(function(item){
           var title = item.volumeInfo.title;
-          finalResult.push({value: title.length>maxCharacterLgth?title.substring(0, maxCharacterLgth):title});
+          finalResult.push({value: title.length>maxCharacterTitleLgth?title.substring(0, maxCharacterTitleLgth):title,
+                            thumbnail: item.volumeInfo.imageLinks.thumbnail, 
+                            description:(item.volumeInfo.description)?item.volumeInfo.description.substring(0, maxDescLength):''});
       });
 
       return finalResult;
@@ -47,18 +60,20 @@ var bloodhoundRemoteConfig = {
 };
 
 var dsRemote = {
-  name: 'best-pictures',
-  display: 'value'
+  name: 'books-to-buy',
+  display: 'value',
+  limit: 8,
+  templates: {
+    suggestion: Handlebars.compile(handlerbarTemplate)
+  }
 };
 
-var typeaheadConfig = {
-    limit: 10;
-};
+var typeaheadConfig = {highlight:false};
 
 React.render(
     <ReactTypeahead bloodhound={bloodhoundRemoteConfig} 
                     datasource={dsRemote}
                     typeahead={typeaheadConfig}
-                    placeHolder="Lets find something cool to read" />,
+                    placeHolder="A remote call + custom template" />,
     document.getElementById('#typeaheadDivRpc')
 );
